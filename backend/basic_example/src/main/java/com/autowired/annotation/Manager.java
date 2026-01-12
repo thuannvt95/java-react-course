24f93b5aa932a68989514bf2b7ecf10d74dc107f
Thuan <thuan1819@gmail.com>
2026-01-12 17:23:08 +0700

    update injection

---
+++ backend/basic_example/src/main/java/com/autowired/annotation/Manager.java
@@ -0,0 +1,23 @@
+package com.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class Manager {
+//    @Autowired
+    private Employee employee;
+
+//    @Autowired
+//    public Manager(Employee employee) {
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}

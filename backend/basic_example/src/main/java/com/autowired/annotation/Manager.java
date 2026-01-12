dc237a99d99948c44803ad24e94da8077b86de93
thuan_nvt <>
2026-01-12 23:07:50 +0700

    update

--- backend/basic_example/src/main/java/com/autowired/annotation/Manager.java
+++ backend/basic_example/src/main/java/com/autowired/annotation/Manager.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@NoArgsConstructor
 public class Manager {
 //    @Autowired
     private Employee employee;
